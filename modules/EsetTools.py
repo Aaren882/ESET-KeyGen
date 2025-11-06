fbefbd0e821342391ffe9779c7fec30d329b14e7
Aaren <aaren.yang@gmail.com>
2025-11-07 04:29:43 +0800

    log

--- modules/EsetTools.py
+++ modules/EsetTools.py
@@ -129,7 +129,7 @@ class EsetKeygen(object):
         self.driver.get('https://home.eset.com/subscriptions/choose-trial')
         
         # Return current page document (html)
-        print(f'Button HTML : "{self.driver.execute_script(f"return document.querySelector('[data-label=""subscription-choose-trial-ehsp-card-button""]')")}"')
+        print(f'Button HTML : {self.driver.execute_script("return document.querySelector('[data-label=""subscription-choose-trial-ehsp-card-button""]')")}')
         uCE(self.driver, f"return {GET_EBAV}('button', 'data-label', 'subscription-choose-trial-ehsp-card-button') != null")
 
         if self.mode == 'ESET HOME':
